27acbcf33ac5680f381dc92f7c1f0d12489289e1
Dawid Stoga PGS <dawid.stoga@gmail.com>
2018-06-07 13:06:18 +0200

    Angular5 quikStart

---
+++ Ang3/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>  {{name}}</h1>`,
+})
+export class AppComponent  { name = "Angular is working in ASP.NET MVC 5" }
