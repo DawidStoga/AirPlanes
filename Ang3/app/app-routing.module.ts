6bea574add2b8e74e17020c47d1f0ad8b5aa9285
Dawid Stoga PGS <dawid.stoga@gmail.com>
2018-06-11 12:30:12 +0200

    Stable version

--- Ang3/app/app-routing.module.ts
+++ Ang3/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { AirplaneComponent } from './airplane/airplane.component';
- import { DashboardComponent} from './dashboard/dashboard.component';
-//import {PlaneDetailsComponent} from "./plane-details/plane-details.component";
+import { AirplaneComponent } from './airplane/airplane.component';
+import { DashboardComponent} from './dashboard/dashboard.component';
+import { PlaneDetailsComponent } from './plane-details/plane-details.component';
 
 
 const routes: Routes = [
- {path: '', redirectTo:'/dashboard', pathMatch: 'full' },
+{path: '', redirectTo:'/dashboard', pathMatch: 'full' },
 {path: 'dashboard', component: DashboardComponent },
-//{path: 'detail/:id', component:PlaneDetailsComponent },
-//{path: 'planes', component: AirplaneComponent },
+{path: 'detail/:id', component:PlaneDetailsComponent },
+{path: 'planes', component: AirplaneComponent },
 ]
   
 
--- Ang3/app/app.component.ts
+++ Ang3/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  { name = "Angular is working in ASP.NET  MVC 5" }
+export class AppComponent {
+  title = 'Airplanes';
+}
